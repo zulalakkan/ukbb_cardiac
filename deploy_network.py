15629d4ce465a4efc2979a11efcd45683be1b6ee
Wenjia Bai <w.bai@imperial.ac.uk>
2017-10-16 16:02:19 +0100

    Updated deploy_network.py

--- deploy_network.py
+++ deploy_network.py
@@ -13,30 +13,28 @@
 # limitations under the License.
 # ==============================================================================
 import os, time, math
-import numpy as np, nibabel as nib
+import numpy as np, nibabel as nib, pandas as pd
 import tensorflow as tf
+from image_utils import *
 
 
 """ Deployment parameters """
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags._global_parser.add_argument('--seq_name', choices=['sa', 'la_2ch', 'la_4ch'],
                                          default='sa', help="Sequence name.")
-tf.app.flags.DEFINE_string('testset_dir', '/tmp/github/test',
+tf.app.flags.DEFINE_string('testset_dir', '/vol/biomedic2/wbai/tmp/github/test',
                            'Path to the test set directory, under which images are organised \
-                            in different subdirectories for each subject.')
-tf.app.flags.DEFINE_string('dest_dir', '/tmp/github/output',
+                            in subdirectories for each subject.')
+tf.app.flags.DEFINE_string('dest_dir', '/vol/biomedic2/wbai/tmp/github/output',
                            'Path to the destination directory, where the segmentations will be saved.')
-tf.app.flags.DEFINE_string('model_path',
-                           '/tmp/github/model/FCN_sa_level5_filter16_22333_Adam_batch2_iter50000_lr0.001.ckpt-50000',
+tf.app.flags.DEFINE_string('model_path', '/vol/biomedic2/wbai/tmp/github/model/FCN_sa_level5_filter16_22333_Adam_batch2_iter50000_lr0.001.ckpt-50000',
                            'Path to the saved trained model.')
-
-tf.app.flags.DEFINE_boolean('process_seq', False, "Process a time sequence of images.")
+tf.app.flags.DEFINE_boolean('process_seq', True, "Process a time sequence of images.")
 tf.app.flags.DEFINE_boolean('save_seg', True, "Save segmentation.")
+tf.app.flags.DEFINE_boolean('clinical_measure', True, "Calculate clinical measures.")
 
 
 if __name__ == '__main__':
-    # Data path
-
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
@@ -47,8 +45,9 @@ if __name__ == '__main__':
         print('Start evaluating on the test set...')
         start_time = time.time()
 
+        # Process each subject subdirectory
         data_list = sorted(os.listdir(FLAGS.testset_dir))
-
+        table = []
         for data in data_list:
             print(data)
             data_dir = os.path.join(FLAGS.testset_dir, data)
@@ -56,12 +55,13 @@ if __name__ == '__main__':
             if FLAGS.process_seq:
                 # Process the temporal sequence
                 image_name = '{0}/{1}.nii.gz'.format(data_dir, FLAGS.seq_name)
-                print(image_name)
 
                 # Read the image
+                print('  Reading {} ...'.format(image_name))
                 nim = nib.load(image_name)
                 image = nim.get_data()
                 X, Y, Z, T = image.shape
+                print('  Segmenting ...'.format(image_name))
 
                 # Intensity rescaling
                 image = rescale_intensity(image, (1, 99))
@@ -72,8 +72,9 @@ if __name__ == '__main__':
                 # Pad the image size to be a factor of 16 so that the downsample and upsample procedures
                 # in the network will result in the same image size at each resolution level.
                 X2, Y2 = int(math.ceil(X / 16.0)) * 16, int(math.ceil(Y / 16.0)) * 16
-                pad_x, pad_y = (X2 - X) / 2, (Y2 - Y) / 2
-                image = np.pad(image, ((pad_x, pad_x), (pad_y, pad_y), (0, 0), (0, 0)), 'constant')
+                x_pre, y_pre = int((X2 - X) / 2), int((Y2 - Y) / 2)
+                x_post, y_post = (X2 - X) - x_pre, (Y2 - Y) - y_pre
+                image = np.pad(image, ((x_pre, x_post), (y_pre, y_post), (0, 0), (0, 0)), 'constant')
 
                 # Process each time frame
                 for t in range(T):
@@ -87,31 +88,61 @@ if __name__ == '__main__':
 
                     # Transpose and crop the segmentation to recover the original size
                     pred_fr = np.transpose(pred_fr, axes=(1, 2, 0))
-                    pred_fr = pred_fr[pad_x:pad_x + X, pad_y:pad_y + Y]
+                    pred_fr = pred_fr[x_pre:x_pre + X, y_pre:y_pre + Y]
                     pred[:, :, :, t] = pred_fr
 
                 # Save the segmentation
-                dest_data_dir = os.path.join(FLAGS.dest_dir, data)
-                if not os.path.exists(dest_data_dir):
-                    os.makedirs(dest_data_dir)
-
                 if FLAGS.save_seg:
+                    print('  Saving segmentation ...')
+                    dest_data_dir = os.path.join(FLAGS.dest_dir, data)
+                    if not os.path.exists(dest_data_dir):
+                        os.makedirs(dest_data_dir)
+
                     nim2 = nib.Nifti1Image(pred, nim.affine)
                     nim2.header['pixdim'] = nim.header['pixdim']
                     nib.save(nim2, '{0}/seg_{1}.nii.gz'.format(dest_data_dir, FLAGS.seq_name))
                     os.system('ln -sf {0}/{1}.nii.gz {2}'.format(data_dir, FLAGS.seq_name, dest_data_dir))
+
+                # Evaluate the clinical measures
+                if FLAGS.clinical_measure and FLAGS.seq_name == 'sa':
+                    print('  Evaluating clinical measures ...')
+
+                    # ED frame defaults to be the first time frame.
+                    # Determine ES frame according to the minimum LV volume.
+                    k = {}
+                    k['ED'] = 0
+                    k['ES'] = np.argmin(np.sum(pred == 1, axis=(0, 1, 2)))
+
+                    # Calculate clinical measures
+                    measure = {}
+                    dx, dy, dz = nim.header['pixdim'][1:4]
+                    volume_per_voxel = dx * dy * dz * 1e-3
+                    density = 1.05
+
+                    for fr in ['ED', 'ES']:
+                        measure[fr] = {}
+                        measure[fr]['LVV'] = np.sum(pred[:, :, :, k[fr]] == 1) * volume_per_voxel
+                        measure[fr]['LVM'] = np.sum(pred[:, :, :, k[fr]] == 2) * volume_per_voxel * density
+                        measure[fr]['RVV'] = np.sum(pred[:, :, :, k[fr]] == 3) * volume_per_voxel
+
+                    line = [measure['ED']['LVV'], measure['ES']['LVV'],
+                            measure['ED']['LVM'],
+                            measure['ED']['RVV'], measure['ES']['RVV']]
+                    table += [line]
             else:
                 # Process ED and ES time frames
+                measure = {}
                 for fr in ['ED', 'ES']:
                     image_name = '{0}/{1}_{2}.nii.gz'.format(data_dir, FLAGS.seq_name, fr)
-                    print(image_name)
 
                     # Read the image
+                    print('  Reading {} ...'.format(image_name))
                     nim = nib.load(image_name)
                     image = nim.get_data()
                     X, Y = image.shape[:2]
                     if image.ndim == 2:
                         image = np.expand_dims(image, axis=2)
+                    print('  Segmenting ...'.format(image_name))
 
                     # Intensity rescaling
                     image = rescale_intensity(image, (1, 99))
@@ -119,8 +150,9 @@ if __name__ == '__main__':
                     # Pad the image size to be a factor of 16 so that the downsample and upsample procedures
                     # in the network will result in the same image size at each resolution level.
                     X2, Y2 = int(math.ceil(X / 16.0)) * 16, int(math.ceil(Y / 16.0)) * 16
-                    pad_x, pad_y = (X2 - X) / 2, (Y2 - Y) / 2
-                    image = np.pad(image, ((pad_x, pad_x), (pad_y, pad_y), (0, 0)), 'constant')
+                    x_pre, y_pre = int((X2 - X) / 2), int((Y2 - Y) / 2)
+                    x_post, y_post = (X2 - X) - x_pre, (Y2 - Y) - y_pre
+                    image = np.pad(image, ((x_pre, x_post), (y_pre, y_post), (0, 0)), 'constant')
 
                     # Transpose the shape to NXYC
                     image = np.transpose(image, axes=(2, 0, 1)).astype(np.float32)
@@ -131,16 +163,45 @@ if __name__ == '__main__':
 
                     # Transpose and crop the segmentation to recover the original size
                     pred = np.transpose(pred, axes=(1, 2, 0))
-                    pred = pred[pad_x:pad_x + X, pad_y:pad_y + Y]
+                    pred = pred[x_pre:x_pre + X, y_pre:y_pre + Y]
 
                     # Save the segmentation
-                    dest_data_dir = os.path.join(FLAGS.dest_dir, data)
-                    if not os.path.exists(dest_data_dir):
-                        os.makedirs(dest_data_dir)
-
                     if FLAGS.save_seg:
+                        print('  Saving segmentation ...')
+                        dest_data_dir = os.path.join(FLAGS.dest_dir, data)
+                        if not os.path.exists(dest_data_dir):
+                            os.makedirs(dest_data_dir)
+
                         nim2 = nib.Nifti1Image(pred, nim.affine)
                         nim2.header['pixdim'] = nim.header['pixdim']
                         nib.save(nim2, '{0}/seg_{1}_{2}.nii.gz'.format(dest_data_dir, FLAGS.seq_name, fr))
                         os.system('ln -sf {0}/{1}_{2}.nii.gz {3}'.format(data_dir, FLAGS.seq_name, fr, dest_data_dir))
+
+                    # Evaluate the clinical measures
+                    if FLAGS.clinical_measure and FLAGS.seq_name == 'sa':
+                        print('  Evaluating clinical measures ...')
+                        dx, dy, dz = nim.header['pixdim'][1:4]
+                        volume_per_voxel = dx * dy * dz * 1e-3
+                        density = 1.05
+
+                        measure[fr] = {}
+                        measure[fr]['LVV'] = np.sum(pred == 1) * volume_per_voxel
+                        measure[fr]['LVM'] = np.sum(pred == 2) * volume_per_voxel * density
+                        measure[fr]['RVV'] = np.sum(pred == 3) * volume_per_voxel
+
+                if FLAGS.clinical_measure and FLAGS.seq_name == 'sa':
+                    line = [measure['ED']['LVV'], measure['ES']['LVV'],
+                            measure['ED']['LVM'],
+                            measure['ED']['RVV'], measure['ES']['RVV']]
+                    table += [line]
+
+        # Save the spreadsheet for the clinical measures
+        if FLAGS.clinical_measure and FLAGS.seq_name == 'sa':
+            column_names = ['LVEDV (mL)', 'LVESV (mL)', 'LVM (g)', 'RVEDV (mL)', 'RVESV (mL)']
+            df = pd.DataFrame(table, index=data_list, columns=column_names)
+            df.to_csv(os.path.join(FLAGS.dest_dir, 'clinical_measure.csv'))
+
         print("Evaluation took {:.3f}s for {:d} subjects.".format(time.time() - start_time, len(data_list)))
+
+
+
